6b3b776be5fff3d68a3f8a702c235384beeb5e11
Matt Wilson <mwilson82@hotmail.com>
2022-09-29 16:17:30 +0000

    fixed bug in pair-programming activity

--- wordsearch.js
+++ wordsearch.js
@@ -1,35 +1,42 @@
-const wordSearch = (letters, word) => { 
-  const horizontalJoin = letters.map(ls => ls.join(''))
-  
-  for (l of horizontalJoin) {
-      if (l.includes(word)) { 
-        return true;
-      }
+const transpose = function(matrix) {
+  let newMatrix = [];
+  for (let y = 0; y < matrix[0].length; y++) {
+    let newArray = [];
+    for (let x = 0; x < matrix.length; x++) {
+      newArray.push(matrix[x][y]);
+    }
+    newMatrix.push(newArray);
   }
+  return newMatrix;
+};
 
-  let vertWordArray = [];
-  for (x = 0; x < letters[0].length; x++) {
-    for (y =0; y < letters.length; y++) {
-      vertWordArray.push(letters[y][x]);
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
   }
-}
-let vertWordString = vertWordArray.join('')
-if (vertWordString.includes(word)) {
-  return true;
-}
-return false;
-}
+  let transPosedLetters = transpose(letters);
+  let verticalJoin = transPosedLetters.map(vs => vs.join(''));
+  for (let v of verticalJoin)
+    if (v.includes(word)) {
+      return true;
+    }
+  return false;
+};
 
 wordSearch([
-['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-], 'LARRY');
+  ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+  ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+  ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+  ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+  ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+  ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+  ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+], 'UUA');
 
-module.exports = wordSearch
+module.exports = wordSearch;
